6fb79d48667d437409d50de3d95ebab4579dcb19
rpscruz <rscruz7@up.edu,ph>
2018-12-03 19:35:28 +0800

    condition update

--- application/models/Conditions_model.php
+++ application/models/Conditions_model.php
@@ -34,16 +34,27 @@
 		}
 
 		public function edit_condition($id){
-			$query = $this->db->get_where('Conditions', array('condition_id' => $id ));
+			//$query = $this->db->get_where('Conditions', array('condition_id' => $id ));
+			//return $query->result();
+
+			$sql = "SELECT * FROM Conditions WHERE condition_id=".$id.";";
+			$this->db->query($sql);
 			return $query->result();
 		}
 
 		public function update_condition(){
-			$data = array(
-				'condition_name' => $this->input->post('condition_name')
-			);
+			//$data = array(
+			//	'condition_name' => $this->input->post('condition_name')
+			//);
 
-			$this->db->where('condition_id', $this->input->post('condition_id'));
-			return $this->db->update('Conditions', $data);
+			//$this->db->where('condition_id', $this->input->post('condition_id'));
+			//return $this->db->update('Conditions', $data);
+
+			$condition_id = $this->input->post('condition_id');
+			$condition_name = $this->input->post('condition_name');
+
+			$sql = "UPDATE Conditions Set condition_name='".$condition_name."' WHERE condition_id=".$condition_id";";
+			$this->db->query($sql);
+			return;
 		}
 	}
