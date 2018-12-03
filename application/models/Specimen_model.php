bd3ac9e2a510892d689305ab72ed022e3418a638
rpscruz <rscruz7@up.edu,ph>
2018-12-03 22:01:00 +0800

    corrected Specimen type-condition exchange

--- application/models/Specimen_model.php
+++ application/models/Specimen_model.php
@@ -44,10 +44,10 @@
 		}
 
 		public function add_specimen(){
-			$patient_id  = $this->input->post('patient_id');
-			$user_id = $this->input->post('user_id');
-			$type_id   = $this->input->post('type_id');
-			$condition_id = $this->input->post('condition_id');
+			$patient_id    = $this->input->post('patient_id');
+			$user_id       = $this->input->post('user_id');
+			$type_id       = $this->input->post('type_id');
+			$condition_id  = $this->input->post('condition_id');
 			$description   = $this->input->post('description');
 
 			$sql = "INSERT INTO Specimen (patient_id, user_id, condition_id, type_id, description) VALUES ('" .$patient_id. "', '" .$user_id. "', '" .$condition_id. "', '" .$type_id. "', '" .$description. "');";
