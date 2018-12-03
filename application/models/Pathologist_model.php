35ffe7304e755bbf80fa941f1c7df68ecd8af659
rpscruz <rscruz7@up.edu,ph>
2018-12-03 20:13:56 +0800

    Pathologist update

--- application/models/Pathologist_model.php
+++ application/models/Pathologist_model.php
@@ -27,11 +27,18 @@
 		}
 		
 		public function edit_pathologist($id) {
-			$query = $this->db->get_where('Pathologist', array('user_id' => $id ));
+			
+			/*$query = $this->db->get_where('Pathologist', array('user_id' => $id ));
+			return $query->result();
+			*/
+
+
+			$query = $this->db->get_where('Patient', array('patient_id' => $id ));
 			return $query->result();
 		}
 
 		public function update_pathologist() {
+			/*
 
 			$data = array(
 				'first_name_path'  => $this->input->post('first_name_path'),
@@ -42,6 +49,22 @@
 
 			$this->db->where('user_id', $this->input->post('user_id'));
 			return $this->db->update('Pathologist', $data);
+			*/
+			$user_id = $this->input->post('user_id');
+
+			$first_name_path   = $this->input->post('first_name_path');
+			$middle_name_path = $this->input->post('middle_name_path');
+			$last_name_path   = $this->input->post('last_name_path');
+			$name_suffix_path = $this->input->post('name_suffix_path');
+
+
+			$sql = "UPDATE Type Set 
+			first_name_path  ='".$first_name_path."', 
+			middle_name_path ='".$middle_name_path."', 
+			last_name_path   ='".$last_name_path."', 
+			name_suffix_path ='".$name_suffix_path."'WHERE user_id=".$user_id.";";
+			$this->db->query($sql);
+			return;
 		}
 		
 	}
